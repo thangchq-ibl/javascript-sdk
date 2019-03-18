430bdcb6a4e2beaad16fca1e5a778e0ff24d2e76
Thang Cai <thangchq@cardano-labo.com>
2019-03-18 09:58:27 +0700

    delete ledger

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import * as client from "./client"
 import * as crypto from "./crypto"
 import * as amino from "./encoder"
 import * as utils from "./utils"
-import Ledger from "./ledger"
 
 const { BncClient } = client
 module.exports = BncClient
 module.exports.crypto = crypto
 module.exports.amino = amino
 module.exports.utils = utils
-module.exports.ledger = Ledger
